d245a8ffd04ee5c38e142d577b63aef7c189944d
imthiyagu07 <imthiyagugithub@gmail.com>
2025-10-17 13:03:48 +0530

    login and logout functionality implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -64,9 +64,30 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.send("Login endpoint");
+    const {email, password} = req.body
+    try {
+        const user = await User.findOne({email})
+        if (!user) return res.status(400).json({message: "Invalid credentials"});
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials"});
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch (err) {
+        console.error("Error in login controller:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
-export const logout = async (req, res) => {
-    res.send("Logout endpoint");
+export const logout = (_, res) => {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({ message: "Logged out successfully"});
 }
