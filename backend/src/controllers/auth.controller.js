780e34bab5bfc4aa37b70a64c4d1ed0d3cdb9ea5
imthiyagu07 <imthiyagugithub@gmail.com>
2025-10-19 18:10:42 +0530

    update profile and check endpoint added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import User from "../models/User.model.js";
 import { ENV } from "../lib/env.js";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const {username, email, password} = req.body
@@ -95,4 +96,22 @@ export const login = async (req, res) => {
 export const logout = (_, res) => {
     res.cookie("jwt", "", {maxAge: 0});
     res.status(200).json({ message: "Logged out successfully"});
+}
+
+export const updateProfile = async (req, res) => {
+    const {profilePic} = req.body;
+    try {
+        if (!profilePic) return res.status(400).json({message: "Profile pic is required"})
+        
+        const userId = req.user._id;
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error in update profile:", error);
+        res.status(500).json({message: "Internal server error"});
+    }
 }
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { ENV } from "../lib/env.js";
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt
+        if (!token) return res.status(401).json({ message: "unauthorized - No token provided"});
+
+        const decoded = jwt.verify(token, ENV.JWT_SECRET)
+        if (!decoded) return res.status(401).json({ message: "unauthorized - Invalid token"});
+
+        const user = await User.findById(decoded.userId).select("-password")
+        if (!user) return res.status(404).json({ message: "User not found"});
+
+        req.user = user;
+        next();
+
+    } catch (err) {
+        console.log("Error in protectRoute middleware:", err);
+        res.status(500).json({ message: "Internal server error"});
+    }
+}
